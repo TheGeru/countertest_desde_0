03c7dcc1882f2c16bde94986ff7c442c8139cfff
Brandon Alonso Salinas <thegeru2prx@gmail.com>
2024-02-16 00:17:20 -0600

    only_commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  HomePageState createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  int cont = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("Contador"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text("Número de clics: "),
+            Text("$cont"),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: _crearBotones(),
+    );
+  }
+
+  Widget _crearBotones() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          FloatingActionButton(
+            onPressed: _sumar,
+            child: const Icon(Icons.add),
+          ),
+          SizedBox(width: 10),
+          FloatingActionButton(
+            onPressed: _reiniciar,
+            child: const Icon(Icons.refresh),
+          ),
+          SizedBox(width: 10),
+          FloatingActionButton(
+            onPressed: _restar,
+            child: const Icon(Icons.remove),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _sumar() {
+    setState(() {
+      cont++;
+    });
+  }
+
+  void _reiniciar() {
+    setState(() {
+      cont = 0;
+    });
+  }
+
+  void _restar() {
+    setState(() {
+      if (cont > 0) {
+        cont--;
+      }
+    });
+  }
+}
